fb05779044e43a7e03e23427466ed10fb46dcd15
Fede <federremu@gmail.com>
2020-10-22 22:23:35 -0300

    inicial commit con nombre de bd y puertos

---
+++ NosqlProjecto/src/main/java/com/paquete/NosqlProjectoApplication.java
@@ -0,0 +1,13 @@
+package com.paquete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NosqlProjectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NosqlProjectoApplication.class, args);
+	}
+
+}
---
+++ NosqlProjecto/src/test/java/com/paquete/NosqlProjectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paquete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NosqlProjectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
